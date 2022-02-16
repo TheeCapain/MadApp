600b4a426c0cc311570533fbaa35a39fc21d6faa
August <Augusthauerslev@outlook.com>
2022-02-16 16:43:04 +0100

    Grocery things

--- src/main/java/com/example/mad2/Grocery/Grocery.java
+++ src/main/java/com/example/mad2/Grocery/Grocery.java
@@ -1,4 +1,15 @@
 package com.example.mad2.Grocery;
 
 public class Grocery {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
--- src/main/java/com/example/mad2/Grocery/GroceryController.java
+++ src/main/java/com/example/mad2/Grocery/GroceryController.java
@@ -1,9 +1,15 @@
 package com.example.mad2.Grocery;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class GroceryController {
 
+    @PostMapping("addGrocery")
+    public String addGrocery(WebRequest webRequest) {
+        return "redirect:/profile";
+    }
 
 }
